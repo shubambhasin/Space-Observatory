54f5e0f40a9965e0f1c07e04bdedf07acf424a79
shubambhasin <shubam.developer@gmail.com>
2020-12-18 16:15:11 +0530

    feat. Added the functionality and UI
    -Styles are added
    -functionalities are added

--- working.js
+++ working.js
@@ -1,39 +1,45 @@
 var apodBtn = document.querySelector(".apod-btn")
 var title = document.querySelector(".title")
-var image =document.querySelector("#image")
-var date =document.querySelector(".date")
+var image = document.querySelector("#image")
+var date = document.querySelector(".date")
 var explain = document.querySelector(".explaination")
 var copyright = document.querySelector(".copyright")
 var clear = document.querySelector("#clear")
+var explainTitle = document.querySelector(".explain-title")
 var key = "J1fbRVDUcTcYu9bR1P4kE4abaolfftDvgzCcYraU"
 var api = "https://api.nasa.gov/planetary/apod?api_key=J1fbRVDUcTcYu9bR1P4kE4abaolfftDvgzCcYraU"
 
-apodBtn.addEventListener('click', function getApodInfo(){
+apodBtn.addEventListener('click', function getApodInfo() {
     fetch(api).then(response => {
         return response.json()
     }).then(data => {
         console.log(data)
         console.log(data.date)
-        date.innerHTML = data.date
+        date.innerHTML = `Date: ${data.date}`
         console.log(data.explanation)
-        explain.innerHTML = data.explanation
+        explain.innerHTML = `${data.explanation}`
         console.log(data.title)
-        title.innerHTML = data.title
+        title.innerHTML = `Title: ${data.title}`
         console.log(data.copyright)
-        copyright.innerHTML= data.copyright
+        copyright.innerHTML = `Copyright: ${data.copyright}`
         console.log(data.url)
         document.getElementById("image").src = data.url
-        
-    })
-} )
+        document.getElementById("image") = data.url
 
-clear.addEventListener("click", function clearEverything(){
+
+    })
+    explainTitle.innerHTML = "Explaination"
+})
+
+clear.addEventListener("click", function clearEverything() {
+
 
     console.log("Everything cleared")
-    date.innerHTML=""
-    explain.innerHTML=""
-      title.innerHTML=""
-  copyright.innerHTML=""
-  image.src=""
+    date.innerHTML = ""
+    explain.innerHTML = ""
+    title.innerHTML = ""
+    copyright.innerHTML = ""
+    image.src = ""
+    explainTitle.innerHTML = ""
 
 })
